c8f408ba03b5e8873ee449ff14e08afb4dcb9661
Pavel Nicovala <pni01@skybet.net>
2020-08-04 09:52:50 +0100

    demo

---
+++ cypress/integration/examples/demo.js
@@ -0,0 +1,10 @@
+context ('Given I access Rose blog', () => {
+
+    it('when I click on first article then I should get Rose as a creator ', () => {
+
+        cy.visit('https://roseblackburn.wixsite.com/website')
+        cy.get('._2ekub > :nth-child(1)').click()
+        cy.get('#content-wrapper').contains('Rose Blackburn')
+
+    });
+})
